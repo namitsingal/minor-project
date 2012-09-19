31a4ed67a08ed15bd07458395c80657a95a90094
namitsingal <namitsingal@gmail.com>
2012-09-19 15:42:18 +0530

    added views.py

--- login/run.py
+++ login/run.py
@@ -1,35 +1,8 @@
 import sys
-from flask import request
-from jinja2 import Template,Environment,FileSystemLoader
-
 sys.path.append('../')
-
-from login import app,Login
+from login import app
 app.debug = True;
 #app.run(port=8080)
+from login import views
 
-env = Environment(loader=FileSystemLoader('templates'))
-
-@app.route('/')
-def index():
-    template = env.get_template('index.html')
-    rendered=template.render()
-    return rendered
-
-
-@app.route('/login',methods=['POST','GET'])
-def login():
-    error = None
-    uname=request.form['username']
-    password=request.form['password']
-    
-    k=Login.query.filter_by(username=uname).first()
-    
-    if(k):
-        if(k.username==uname and k.password==password):
-	    return 'successfull login' 
-	else:
-	    return 'wrong password'
-    else: 
-	return 'username does not exist'
 app.run(port=8080)
