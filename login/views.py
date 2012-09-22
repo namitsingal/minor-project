ab6cdf2e59b20d1eef060410fd83be5f5d3632e3
namitsingal <namitsingal@gmail.com>
2012-09-22 18:42:51 +0530

    register

--- login/views.py
+++ login/views.py
@@ -1,7 +1,7 @@
 from login import app
 from flask import request,session,redirect,escape,url_for
 from jinja2 import Template,Environment,FileSystemLoader
-from login import Login
+from login import Login,db
 env = Environment(loader=FileSystemLoader('templates'))
 
 @app.route('/login')
@@ -46,6 +46,18 @@ def register():
         rendered=template.render()
         return rendered
 
+@app.route('/register',methods=['POST','GET',''])
+def register():
+	uname=request.form['username']
+        password=request.form['passwords']
+	email=request.form['email']
+	user=Login(uname,password,email)
+	db.session.add(user)
+	db.session.commit();	
+	session['username']=uname
+	return redirect('/index')
 
-app.secret_key='\xe6m\x897\xeec \x88\x9e\xc8\xdd\x99\xd2\xec\xf0\x0f\x88&\x00\x00psb\x10'
+
+
+app.secret_key='\xe6m\x897\xeec\x88\x9e\xc8\xdd\x99\xd2\xec\xf0\x0f\x88\x00\x00psb\x10'
 
